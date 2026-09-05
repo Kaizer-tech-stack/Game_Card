6d2384c456b77e9a42345776f30cbbf42830abd9
Kaizer-tech-stack <cloud09magallanes@gmail.com>
2026-09-05 18:20:30 +0800

    I update the code for the matching cards and debug some codes

--- Game_Card_Memory/Game_Card.js
+++ Game_Card_Memory/Game_Card.js
@@ -32,14 +32,7 @@ const cardValues = [
   "watermelon",
 ];
 
-card.forEach(function (card, index) {
-  card.addEventListener("click", function () {
-    card.innerHTML = cardValues[index]; // card values for each card
-    card.classList.add("flipped");
-  });
-});
-
-let flippedCards = []; // Array to store the flipped Cards
+/*let flippedCards = []; // Array to store the flipped Cards
 card.forEach(function (card) {
   card.addEventListener("click", function () {
     flippedCards.push(card);
@@ -48,6 +41,41 @@ card.forEach(function (card) {
         console.log("Match Found!");
       } else {
         console.log("No Match!");
+        const cardToHide = flippedCards;
+        setTimeout(function () {
+          cardToHide.forEach(function (card) {
+            card.innerHTML = "";
+            card.classList.remove("flipped");
+          });
+        }, 1000);
+      }
+      flippedCards = [];
+    }
+  });
+});*/
+
+let flippedCards = [];
+card.forEach(function (card, index) {
+  card.addEventListener("click", function () {
+    if (!flippedCards.includes(card)) {
+      card.innerHTML = cardValues[index]; // card values for each card
+      card.classList.add("flipped");
+      flippedCards.push(card);
+
+      if (flippedCards.length === 2) {
+        if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+          console.log("Match Found!");
+        } else {
+          console.log("No Match!");
+          const cardToHide = flippedCards;
+          setTimeout(function () {
+            cardToHide.forEach(function (card) {
+              card.innerHTML = "";
+              card.classList.remove("flipped");
+            });
+          }, 1000);
+        }
+        flippedCards = [];
       }
     }
   });
