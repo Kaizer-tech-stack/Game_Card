5d62be7d62ed0ff219bed691669df47d177e0dbd
Kaizer-tech-stack <cloud09magallanes@gmail.com>
2026-09-05 15:57:37 +0800

    I add a flipped cards function

--- Game_Card_Memory/Game_Card.js
+++ Game_Card_Memory/Game_Card.js
@@ -7,7 +7,7 @@ console.log(card);
 
 card.forEach(function (card) {
   card.addEventListener("click", function () {
-    card.style.backgroundColor = "red";
+    //card.style.backgroundColor = "red";
     console.log(card.classList);
     console.log(card.classList[1]);
   });
@@ -31,8 +31,24 @@ const cardValues = [
   "watermelon",
   "watermelon",
 ];
+
 card.forEach(function (card, index) {
   card.addEventListener("click", function () {
-    card.innerHTML = cardValues[index];
+    card.innerHTML = cardValues[index]; // card values for each card
+    card.classList.add("flipped");
+  });
+});
+
+let flippedCards = []; // Array to store the flipped Cards
+card.forEach(function (card) {
+  card.addEventListener("click", function () {
+    flippedCards.push(card);
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].innerHTML === flippedCards[1].innerHTML) {
+        console.log("Match");
+      } else {
+        console.log("No Match");
+      }
+    }
   });
 });
